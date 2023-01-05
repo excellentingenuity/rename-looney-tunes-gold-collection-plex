297b663e06eefbb60959ff7346c091774c809f12
James Johnson <james.johnson@excellentingenuity.com>
2023-01-05 16:57:50 -0700

    Changed the script and updated the file and episode list
    start of updates to make it work

--- main.js
+++ main.js
@@ -3,11 +3,11 @@
 const fs = require('fs');
 const path = require('path');
 const CSV = require('csv-string');
-const Finder = require('fs-finder');
 const chalk = require('chalk');
 const argv = require('minimist')(process.argv.slice(2));
 
-const tvdbPath = path.resolve('./tvdb.csv');
+//const tvdbPath = path.resolve('./fix2.csv');
+const tvdbPath = path.resolve('./Looney Tunes Golden Collection Episode List.csv');
 const libraryPath = path.resolve(argv.library);
 
 if (!argv.run) {
@@ -18,35 +18,60 @@ console.log(`Loading ${libraryPath}`);
 console.log(`Loading ${tvdbPath}`);
 console.log('');
 
-const files = Finder.from(libraryPath).findFiles();
+const files = fs.readdirSync(libraryPath);
 const content = fs.readFileSync(tvdbPath, 'utf8');
 const tvdb = CSV.parse(content);
 
 for (const episodeRow of tvdb) {
-  const year = episodeRow[0].split(' x ')[0];
-  const number = episodeRow[0].split(' x ')[1];
-  const name = episodeRow[1];
-  const plex = `S${year}E${number} - ${name}`;
 
-  const matchingFilePath = files.find((file) => {
-    return new RegExp(name, 'i').test(file);
+  var originalName = String(episodeRow[0] + '.mkv').trim();
+  //console.log('original name is: ' + originalName);
+  var newName = episodeRow[1];
+  
+  // match the file in the library path to the original file name in the CSV file
+  const matchingFile = files.filter(function (file) {
+    // //console.log(typeof file);
+
+    // var areEqual = false;
+    
+    // if(file === originalName){
+    //   areEqual = true;
+    //   console.log('file is: ' + file);
+    //   console.log('file length is :' + file.length);
+    //   console.log('original name is: ' + originalName);
+    //   console.log('file length is :' + originalName.length);
+    //   console.log('eval of file comparison is: ' + areEqual);
+    // }
+    
+    return file === originalName;
   });
 
-  if (matchingFilePath) {
+  let matchingFilePath = false;
+  // get the full path to the file for renaming
+  if(Object.keys(matchingFile).length != 0 && matchingFile.constructor != Object)
+  {
+    //console.log(typeof matchingFile);
+    //console.log('matchingFile is: ' + matchingFile);
+    matchingFilePath = path.resolve(libraryPath + '/' + matchingFile);
+  }
+  
+  if(matchingFilePath != false) {
+    //console.log('matchingFilePath is: ' + matchingFilePath);
+  }
+  
+  if (matchingFilePath != false) {
+    //console.log('inside matchingFilePath');
     const pathSegments = matchingFilePath.split('/');
     const fileName = pathSegments[pathSegments.length - 1];
     const fileExt = path.extname(fileName);
-    const newFileName = `${plex}${fileExt}`;
-    const newFilePath = matchingFilePath.replace(fileName, newFileName);
+    const newFilePath = matchingFilePath.replace(fileName, newName + '.mkv');
+
+    console.log(`Renaming ${chalk.blue(matchingFilePath)} to ${chalk.green(newFilePath)}`);
+    console.log('');
 
     if (argv.run === true) {
-      console.log(`Renaming ${chalk.blue(matchingFilePath)} to ${chalk.green(newFilePath)}`);
-      console.log('');
       fs.renameSync(matchingFilePath, newFilePath);
-    } else {
-      console.log(`Renaming ${chalk.blue(matchingFilePath)} to ${chalk.green(newFilePath)}`);
-      console.log('');
-    }
+    } 
   }
 }
 
